ea90c4b310bd4ed0da055202d4e40b0f3dafcdf7
MuskMalone <44629591+MuskMalone@users.noreply.github.com>
2024-09-08 20:14:27 +0800

    second stone
    for debug its kinda scuffed but you need to download the shaderc_combined.lib file ive sent on telegram and put it into vendor/cross-platform/shaderc/libshaderc/Debug

--- vendor/cross-platform/yaml-cpp/util/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/terra/OneDrive/Documents/references/y3/MoravaEngine/vendor/cross-platform/yaml-cpp/util
-# Build directory: C:/Users/terra/OneDrive/Documents/references/y3/MoravaEngine/vendor/cross-platform/yaml-cpp/util
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
