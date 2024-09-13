405c0ddc9dc8581375b8254bcde86d10a3150d42
xavier <xav.choa@gmail.com>
2024-09-14 01:36:28 +0800

    Add low level API

---
+++ ImaGE/source/Graphics/RenderAPI.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "VertexArray.h"
+
+namespace Graphics{
+	class RenderAPI{
+	public:
+		void Init();
+		void SetViewport(unsigned int x, unsigned int y, unsigned int width, unsigned int height);
+		void SetClearColor(const glm::vec4& color);
+		void Clear();
+
+		void DrawIndices(std::shared_ptr<VertexArray> const& vao, unsigned int idxCount);
+		void DrawLines(std::shared_ptr<VertexArray> const& vao, unsigned int vtxCount);
+
+		void SetLineWidth(float width);
+	};
+}
+
