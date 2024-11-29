c7224800baa20f0bdebc5c29686a11add3489a45
Erniesmash <ernestcheo@gmail.com>
2024-11-29 20:51:07 +0800

    For Xavier

---
+++ ImaGE-Script/source/InteractableObject/SafeButtons.cs
@@ -0,0 +1,39 @@
+﻿using IGE.Utils;
+using System;
+
+public class SafeButtons : Entity
+{
+  void Start()
+  {
+
+  }
+
+  void Update()
+  {
+    
+  }
+
+  public void OnPointerEnter()
+  {
+    string tag = InternalCalls.GetTag(mEntityID);
+    Console.WriteLine("From MONO Enter" + tag);
+  }
+
+  public void OnPointerExit()
+  {
+    string tag = InternalCalls.GetTag(mEntityID);
+    Console.WriteLine("From MONO Exit" + tag);
+  }
+
+  public void OnPointerDown()
+  {
+    string tag = InternalCalls.GetTag(mEntityID);
+    Console.WriteLine("From MONO Down" + tag);
+  }
+
+  public void OnPointerUp()
+  {
+    string tag = InternalCalls.GetTag(mEntityID);
+    Console.WriteLine("From MONO Up" + tag);
+  }
+}
