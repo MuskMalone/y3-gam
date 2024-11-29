4317f4985ed6e6216584110ef50db6344f42c482
Erniesmash <ernestcheo@gmail.com>
2024-11-29 19:20:11 +0800

    Progress Save

---
+++ ImaGE-Script/source/InteractableObject/SuspiciousPainting.cs
@@ -0,0 +1,29 @@
+﻿using IGE.Utils;
+
+public class SuspiciousPainting : Entity
+{
+  // Script to be placed in the SuspiciousPainting Entity (Parent)
+  public PlayerInteraction playerInteraction;
+  public Entity removePaintingUI;
+
+  void Start()
+  {
+    removePaintingUI?.SetActive(false);
+  }
+
+  void Update()
+  {
+    if (IsActive())
+    {
+      bool isPaintingHit = playerInteraction.RayHitString == InternalCalls.GetTag(mEntityID);
+      if (Input.GetMouseButtonTriggered(0) && isPaintingHit)
+      {
+        SetActive(false);
+        removePaintingUI.SetActive(false);
+        return;
+      }
+
+      removePaintingUI.SetActive(isPaintingHit);
+    }
+  }
+}
