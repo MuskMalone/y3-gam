0621f4e45760c8afddd241ac3d3feaf5f3ce0d10
Erniesmash <ernestcheo@gmail.com>
2024-11-30 17:13:09 +0800

    Altar Painting Interaction

---
+++ ImaGE-Script/source/InteractableObject/AltarPaintingFrame.cs
@@ -0,0 +1,35 @@
+﻿using IGE.Utils;
+using System.Numerics;
+
+public class AltarPaintingFrame : Entity
+{
+  public PlayerInteraction playerInteraction;
+  public Entity altarPaintingFrameUI;
+  public string[] paintingDialogue;
+  public Dialogue dialogueSystem;
+
+  private bool paintingFlag = false;
+
+  void Start()
+  {
+    altarPaintingFrameUI?.SetActive(false);
+  }
+
+  void Update()
+  {
+    bool isPaintingHit = playerInteraction.RayHitString == InternalCalls.GetTag(mEntityID);
+    if (Input.GetMouseButtonTriggered(0) && isPaintingHit && !paintingFlag)
+    {
+      dialogueSystem.SetDialogue(paintingDialogue, new Dialogue.Emotion[] { Dialogue.Emotion.Neutral, Dialogue.Emotion.Thinking });
+      paintingFlag = true;
+      return;
+    }
+
+    altarPaintingFrameUI.SetActive(isPaintingHit);
+
+    if (!isPaintingHit)
+    {
+      paintingFlag = false;
+    }
+  }
+}
