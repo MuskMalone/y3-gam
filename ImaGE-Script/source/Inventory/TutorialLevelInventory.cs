1a5be42875bf620351c07475e30e3bfa3bb404ec
wen <113166960+doopermane@users.noreply.github.com>
2024-12-01 04:36:01 +0800

    port stuff for picture align

--- ImaGE-Script/source/Inventory/TutorialLevelInventory.cs
+++ ImaGE-Script/source/Inventory/TutorialLevelInventory.cs
@@ -77,7 +77,7 @@ public class TutorialLevelInventory : Entity
         tutorialPaintingUI?.SetActive(false);
 
 
-        Vector3 startPosition = new Vector3(16.6f, -8.9f, 0f);
+        Vector3 startPosition = new Vector3(12.0f, -4.0f, 0f);
         InternalCalls.SetPosition(tutorialPaintingUI.mEntityID, ref startPosition);
     }
 
