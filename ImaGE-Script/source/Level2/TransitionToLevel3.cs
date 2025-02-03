de063b0052030c2e0044be02b4265cf10bb1e55b
wen <113166960+doopermane@users.noreply.github.com>
2025-02-03 09:24:14 +0800

    transition to level 3

---
+++ ImaGE-Script/source/Level2/TransitionToLevel3.cs
@@ -0,0 +1,48 @@
+﻿/******************************************************************************/
+/*!
+\par        Image Engine
+\file       .cs
+
+\author     
+\date       
+
+\brief      
+
+
+Copyright (C) 2024 DigiPen Institute of Technology. Reproduction
+or disclosure of this file or its contents without the prior
+written consent of DigiPen Institute of Technology is prohibited.
+*/
+/******************************************************************************/
+
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using IGE.Utils;
+
+public class TransitionToLevel3 : Entity
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.NUM_4))
+        {
+            InternalCalls.SetCurrentScene("..\\Assets\\Scenes\\Level3.scn");
+        }
+    }
+}
+
+
