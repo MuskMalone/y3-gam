f17e59a6c5b7f75c36c610efa3663f3a696d34d2
Yuki <yukiyz.ng@gmail.com>
2025-02-09 22:05:24 +0800

    TutorialPausemenu

---
+++ ImaGE-Script/source/Tutorial/PauseMenu.cs
@@ -0,0 +1,53 @@
+﻿using IGE.Utils;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+public class PauseMenu : Entity
+{
+    public Entity imageEntity; // The entity containing the pause menu UI
+    private bool isPaused = false; // Tracks pause state
+    private PlayerMove playerMove; // Reference to player movement script
+
+    void Start()
+    {
+        if (imageEntity != null)
+        {
+            imageEntity.SetActive(false); // Ensure menu is off at the start
+        }
+
+        playerMove = FindObjectOfType<PlayerMove>(); // Get the player movement script
+        if (playerMove == null) Debug.LogError("PlayerMove component not found!");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P)) // Press 'P' to toggle pause
+        {
+            isPaused = !isPaused;
+
+            if (imageEntity != null)
+            {
+                imageEntity.SetActive(isPaused);
+            }
+
+            if (playerMove != null)
+            {
+                if (isPaused)
+                {
+                    playerMove.FreezePlayer(); // Freeze movement
+                }
+                else
+                {
+                    playerMove.UnfreezePlayer(); // Resume movement
+                }
+            }
+        }
+    }
+}
