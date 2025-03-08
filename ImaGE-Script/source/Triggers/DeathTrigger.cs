e0fdc5709b69cb2ee133e62ef05d258aec8108fa
wen <113166960+doopermane@users.noreply.github.com>
2025-03-08 23:31:42 +0800

    stuff

---
+++ ImaGE-Script/source/Triggers/DeathTrigger.cs
@@ -0,0 +1,53 @@
+﻿/******************************************************************************/
+/*!
+\par        Image Engine
+\file       .cs
+
+\author     
+\date       
+
+\brief      
+
+
+Copyright (C) 2024 DigiPen Institute of Technology. Reproduction
+or disclosure of this file or its contents without the prior
+written consent of DigiPen Institute of Technology is prohibited.
+*/
+/******************************************************************************/
+
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using IGE.Utils;
+
+public class DeathTrigger : Entity
+{
+    public Entity deathbox;
+    public Entity player;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (InternalCalls.OnTriggerEnter(deathbox.mEntityID, player.mEntityID))
+        {
+            Console.WriteLine("death triggered");
+        }
+    }
+
+
+}
+
+
