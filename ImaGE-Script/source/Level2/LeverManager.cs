6d70e7131e5d95d26dda7e0e5c82e3c594161867
Yuki <yukiyz.ng@gmail.com>
2025-03-13 14:46:22 +0800

    ladder, middle part and some assets and materials

--- ImaGE-Script/source/Level2/LeverManager.cs
+++ ImaGE-Script/source/Level2/LeverManager.cs
@@ -9,9 +9,21 @@ public class LeverManager : Entity
     public Entity leverLight3;
     public Entity leverLight4;
     public Entity leverLight5;
+
+    public Entity leverBallGray1, leverBallGold1;
+    public Entity leverBallGray2, leverBallGold2;
+    public Entity leverBallGray3, leverBallGold3;
+    public Entity leverBallGray4, leverBallGold4;
+    public Entity leverBallGray5, leverBallGold5;
+
+    public Entity playerCamera;
+    public Entity tableCamera;
+    public PlayerMove playerMove;
     //public Entity door; // The door to unlock
     private int leversPulled = 0;
     private int totalLevers = 5; // Set total number of levers required
+    private float cameraSwitchTime = 0f;
+    private float switchDuration = 4.0f;
 
     public LeverManager() : base() { }
 
@@ -24,44 +36,86 @@ public class LeverManager : Entity
         InternalCalls.SetLightIntensity(leverLight3.mEntityID, 0.0f);
         InternalCalls.SetLightIntensity(leverLight4.mEntityID, 0.0f);
         InternalCalls.SetLightIntensity(leverLight5.mEntityID, 0.0f);
+
+        leverBallGray1.SetActive(true);
+        leverBallGray2.SetActive(true); leverBallGray3.SetActive(true);
+        leverBallGray4.SetActive(true); leverBallGray5.SetActive(true);
+        leverBallGold1.SetActive(false);
+        leverBallGold2.SetActive(false); leverBallGold3.SetActive(false); leverBallGold4.SetActive(false);
+        leverBallGold5.SetActive(false);
     }
+
 
     public void LeverPulled()
     {
         leversPulled++;
         Console.WriteLine($"Lever pulled! {leversPulled}/{totalLevers} levers activated.");
+        if (playerMove != null)
+        {
+            playerMove.FreezePlayer(); // Freeze player
+        }
 
-        // Light up a corresponding light
-        switch (leversPulled)
+
+        SetTableCameraAsMain(); // Instantly switch camera
+
+        ActivateLeverLight(leversPulled); // Activate the correct lever light
+
+        SetPlayerCameraAsMain(); // Instantly switch back
+
+        if (playerMove != null)
+        {
+            playerMove.UnfreezePlayer(); // Unfreeze player
+        }
+    }
+
+    private void ActivateLeverLight(int leverIndex)
+    {
+        switch (leverIndex)
         {
             case 1:
                 InternalCalls.SetLightIntensity(leverLight1.mEntityID, 5.0f);
-                Console.WriteLine("Light 1 turned on!");
+                leverBallGray1.SetActive(false);
+                leverBallGold1.SetActive(true);
                 break;
             case 2:
                 InternalCalls.SetLightIntensity(leverLight2.mEntityID, 5.0f);
-                Console.WriteLine("Light 2 turned on!");
+                leverBallGray2.SetActive(false);
+                leverBallGold2.SetActive(true);
                 break;
             case 3:
                 InternalCalls.SetLightIntensity(leverLight3.mEntityID, 5.0f);
-                Console.WriteLine("Light 3 turned on!");
+                leverBallGray3.SetActive(false);
+                leverBallGold3.SetActive(true);
                 break;
             case 4:
                 InternalCalls.SetLightIntensity(leverLight4.mEntityID, 5.0f);
-                Console.WriteLine("Light 4 turned on!");
+                leverBallGray4.SetActive(false);
+                leverBallGold4.SetActive(true);
                 break;
             case 5:
                 InternalCalls.SetLightIntensity(leverLight5.mEntityID, 5.0f);
-                Console.WriteLine("Light 5 turned on!");
-                UnlockDoor();
+                leverBallGray5.SetActive(false);
+                leverBallGold5.SetActive(true);
+                UnlockFrag();
                 break;
         }
     }
 
-    private void UnlockDoor()
+    private void SetPlayerCameraAsMain()
+    {
+        Console.WriteLine("Entered SetCamera as Main");
+        InternalCalls.SetTag(playerCamera.mEntityID, "MainCamera");
+        InternalCalls.SetTag(tableCamera.mEntityID, "hexTableCamera");
+    }
+
+    private void SetTableCameraAsMain()
+    {
+        InternalCalls.SetTag(playerCamera.mEntityID, "hexTableCamera");
+        InternalCalls.SetTag(tableCamera.mEntityID, "MainCamera");
+    }
+
+    private void UnlockFrag()
     {
         Console.WriteLine("All levers pulled! The door unlocks...");
-        //InternalCalls.PlaySound(door.mEntityID, "UnlockDoorSound");
-        //InternalCalls.PlayAnimation(door.mEntityID, "DoorOpen");
     }
 }
