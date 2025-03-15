64982d302b022790e3f6350b4d6943433a5d5ec1
Cheng En <chengenlau4912@gmail.com>
2025-03-16 00:48:46 +0800

    PitDoor animation

---
+++ ImaGE-Script/source/InteractableObject/PitDoor.cs
@@ -0,0 +1,46 @@
+﻿using IGE.Utils;
+
+public class PitDoor : Entity
+{
+  public PlayerInteraction playerInteraction;
+  public Entity gapCollider;
+  public string openAnimName;
+
+  private string entityTag;
+  private bool playerInteracted = false;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    entityTag = InternalCalls.GetTag(mEntityID);
+    gapCollider.SetActive(true);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    // if player hasn't interacted with door
+    if (!playerInteracted)
+    {
+      if (Input.GetMouseButtonTriggered(0) && playerInteraction.RayHitString == entityTag)
+      {
+        InternalCalls.PlayAnimation(mEntityID, openAnimName);
+        InternalCalls.DestroyEntity(gapCollider.mEntityID);
+        playerInteracted = true;
+      }
+    }
+    // else animation in progress
+    else
+    {
+      // update collider
+      InternalCalls.UpdatePhysicsToTransform(mEntityID);
+
+      if (!InternalCalls.IsPlayingAnimation(mEntityID))
+      {
+        Destroy(this);  // destroy this script
+      }
+    }
+  }
+}
+
+
