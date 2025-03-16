dbfd97d6186e2ef7217026c7d15acc83caaabe4c
1SilentClap <44629862+HanQinDing@users.noreply.github.com>
2025-03-17 03:49:11 +0800

    change script

--- ImaGE-Script/source/Painting/PaintingAlignUILevel2.cs
+++ ImaGE-Script/source/Painting/PaintingAlignUILevel2.cs
@@ -73,13 +73,16 @@ public class PaintingAlignUILevel2 : Entity
         {
             if (pictureAlignScript.IsAligned())
             {
-                if (leverTwoScript.LeverPulled())
+                if (pictureAlignScript.preventAlignment)
                 {
+                  if (leverTwoScript.LeverPulled())
+                  {
                     pictureAlignScript.preventAlignment = false;
-                }
-                else if (Input.GetMouseButtonDown(0) && !dialogueScript.isInDialogueMode)
-                {
-                  dialogueScript.SetDialogue(lever2Dialogue, new TutorialDialogue.Emotion[] { TutorialDialogue.Emotion.Thinking });
+                  }
+                  else if (Input.GetMouseButtonDown(0) && !dialogueScript.isInDialogueMode)
+                  {
+                    dialogueScript.SetDialogue(lever2Dialogue, new TutorialDialogue.Emotion[] { TutorialDialogue.Emotion.Thinking });
+                  }
                 }
 
                 bigPaintingUI.SetActive(false);
