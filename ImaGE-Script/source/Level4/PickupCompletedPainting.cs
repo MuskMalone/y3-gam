dffd9e271f550360c7de2a414f684cbd426e7f49
Erniesmash <ernestcheo@gmail.com>
2025-03-16 20:46:35 +0800

    Progress Save

---
+++ ImaGE-Script/source/Level4/PickupCompletedPainting.cs
@@ -0,0 +1,41 @@
+﻿using IGE.Utils;
+using System;
+using System.Numerics;
+using System.Text;
+
+public class PickupCompletedPainting : Entity
+{
+  public BlackBorder blackBorder;
+  public PlayerInteraction playerInteraction;
+  public Entity EToPickUpUI;
+
+  public PickupCompletedPainting() : base()
+  {
+
+  }
+
+  void Start()
+  {
+    EToPickUpUI?.SetActive(false);
+  }
+
+  void Update()
+  {
+    if (IsActive())
+    {
+      bool isPaintingHit = playerInteraction.RayHitString == InternalCalls.GetTag(mEntityID);
+      if (Input.GetKeyTriggered(KeyCode.E) && isPaintingHit)
+      {
+        InternalCalls.PlaySound(mEntityID, "PickupObjects");
+        BeginCutscene();
+        SetActive(false);
+      }
+      EToPickUpUI.SetActive(isPaintingHit);
+    }
+  }
+
+  void BeginCutscene()
+  {
+    blackBorder.DisplayBlackBorders();
+  }
+}
